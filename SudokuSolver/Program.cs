8b6ecdd494018a940d893bd973f1d05d970a5a52
betato <battlestargatticus@gmail.com>
2016-07-05 16:28:30 -0400

    Create backtracking algorithm

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static Solver solver = new Solver();
+
+        static void Main(string[] args)
+        {
+            int[][] puzzle = new int[][]{
+            new int[] {0,0,3,0,2,0,6,0,0},
+            new int[] {9,0,0,3,0,5,0,0,1},
+            new int[] {0,0,1,8,0,6,4,0,0},
+            new int[] {0,0,8,1,0,2,9,0,0},
+            new int[] {7,0,0,0,0,0,0,0,8},
+            new int[] {0,0,6,7,0,8,2,0,0},
+            new int[] {0,0,2,6,0,9,5,0,0},
+            new int[] {8,0,0,2,0,3,0,0,9},
+            new int[] {0,0,5,0,1,0,3,0,0}};
+
+            solver.SolvePuzzle(puzzle);
+            WritePuzzle(puzzle);
+            Console.ReadLine();
+        }
+
+        public static void WritePuzzle(int[][] puzzle)
+        {
+            foreach (int[] row in puzzle)
+            {
+                foreach (int num in row)
+                {
+                    Console.Write(num);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ SudokuSolver/Solver.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Solver
+    {
+        public bool SolvePuzzle(int[][] puzzle)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // Don't calculate non-empty squares
+                    if (puzzle[i][j] != 0)
+                    {
+                        continue;
+                    }
+
+                    // Try all possible numbers
+                    for (int num = 1; num <= 9; num++)
+                    {
+                        // Check if number is valid for the given position
+                        if (ValidNumber(num, i, j, puzzle))
+                        {
+                            puzzle[i][j] = num;
+                            if (SolvePuzzle(puzzle))
+                            {
+                                // Number fits
+                                return true;
+                            }
+                            else
+                            {
+                                // The number doesn't work here
+                                puzzle[i][j] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool ValidNumber(int number, int row, int column, int[][] puzzle)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                // Check if row or column already has number
+                if (puzzle[row][i] == number || puzzle[i][column] == number)
+                {
+                    return false;
+                }
+            }
+            // Check if 3x3 grid already has number
+            int xBox = (int)(column / 3) * 3;
+            int yBox = (int)(row / 3) * 3;
+            for (int i = xBox; i < xBox + 3; i++)
+            {
+                for (int j = yBox; j < yBox + 3; j++)
+                {
+                    if (puzzle[j][i] == number)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
